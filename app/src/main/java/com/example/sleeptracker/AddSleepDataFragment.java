38291187112f9de38646b95d45f989e6b052088f
MrMnt <73601367+MrMnt@users.noreply.github.com>
2021-08-20 12:23:18 +0300

    UI for adding sleep data

--- app/src/main/java/com/example/sleeptracker/AddSleepDataFragment.java
+++ app/src/main/java/com/example/sleeptracker/AddSleepDataFragment.java
@@ -1,15 +1,36 @@
 package com.example.sleeptracker;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AddSleepDataFragment extends Fragment {
 
     private static final String TAG = "AddSleepDataFragment";
-    
+
+    private TextView textSleepStart;
+    private TextView textSleepEnd;
+    private TextView textSleepDuration;
+
     public AddSleepDataFragment() {
         super(R.layout.fragment_add_sleep_data);
         Log.d(TAG, "AddSleepDataFragment: ");
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    private void initViews(){
+        textSleepStart = getView().findViewById(R.id.text_sleep_start_time);
+        textSleepEnd = getView().findViewById(R.id.text_sleep_end_time);
+        textSleepDuration = getView().findViewById(R.id.text_sleep_duration);
     }
 }
